5f10bd40a64f4c8aa80dd7332e1b59b608c306cd
Moses <moseschism@my.unt.edu>
2025-02-24 13:26:43 -0800

    SettingUpProfileAndStartPage

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerProfile : MonoBehaviour
+{
+    public InputField playerNameInput;
+    public Text welcomeText;
+    public GameObject profilePanel;
+
+    void Start()
+    {
+        LoadProfile();
+    }
+
+    public void SaveProfile()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            PlayerPrefs.SetString("PlayerName", playerName);
+            PlayerPrefs.Save();
+            welcomeText.text = "Welcome, " + playerName + "!";
+            profilePanel.SetActive(false);  // Hide profile setup after saving
+        }
+    }
+
+    void LoadProfile()
+    {
+        if (PlayerPrefs.HasKey("PlayerName"))
+        {
+            string savedName = PlayerPrefs.GetString("PlayerName");
+            welcomeText.text = "Welcome back, " + savedName + "!";
+            profilePanel.SetActive(false); // Hide profile setup if already saved
+        }
+    }
+}
